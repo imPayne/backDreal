f79e056b935623f6d90b3546b830b3f1721ffcb3
monchablon hugo <mhugo@localhost.localdomain>
2021-11-19 13:07:19 +0100

    corrections for the request sql (tableHandler.js)

--- API/tableHandler.js
+++ API/tableHandler.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "yornletard",
-  password: "8ebea8Lx1",
+  user: "root",
+  password: "MatferDreal61",
   database: "dreal",
 });
 
@@ -11,32 +11,32 @@ const queries = [
   {
     name: "building",
     sql:
-      "CREATE TABLE IF NOT EXISTS Buildings (" +
+      "CREATE TABLE IF NOT EXISTS building (" +
       "building_id int(11) NOT NULL, " +
       "posX int(11) DEFAULT NULL, " +
       "posY int(11) DEFAULT NULL, " +
       "width int(11) DEFAULT NULL, " +
       "height int(11) DEFAULT NULL, " +
-      "PRIMARY KEY (building_id))",
+      "PRIMARY KEY (building_id));",
   },
   {
     name: "storage",
     sql:
-      "CREATE TABLE IF NOT EXISTS Storage ( " +
+      "CREATE TABLE IF NOT EXISTS storage ( " +
       "id int(11) DEFAULT NULL, " +
       "zone_id int(11) DEFAULT NULL, " +
-      "level varchar(100) DEFAULT NULL, " +
-      "storage varchar(100) DEFAULT NULL, " +
+      "`level` varchar(100) DEFAULT NULL, " +
+      "`storageData` varchar(100) DEFAULT NULL, " +
       "StorageMassBois int(11) DEFAULT NULL, " +
       "StorageMassPlastique int(11) DEFAULT NULL, " +
       "StorageMassPD int(11) DEFAULT NULL, " +
       "UNIQUE KEY id (id), Key zone_id (zone_id), " +
-      "CONSTRAINT Storage_ibfk FOREIGN KEY (zone_id) REFERENCES Zone (id) ON DELETE CASCADE ON UPDATE CASCADE)",
+      "CONSTRAINT `storageFK1` FOREIGN KEY (`zone_id`) REFERENCES `zone` (`id`) ON DELETE CASCADE ON UPDATE CASCADE);",
   },
   {
     name: "zone",
     sql:
-      "CREATE TABLE IF NOT EXISTS Zone (" +
+      "CREATE TABLE IF NOT EXISTS zone (" +
       "building_id int(11) NOT NULL, " +
       "id int(11) NOT NULL AUTO_INCREMENT, " +
       "zone_posX int(11) NOT NULL, " +
@@ -47,7 +47,7 @@ const queries = [
       "`column` varchar(45) DEFAULT NULL, " +
       "PRIMARY KEY (id), " +
       "KEY building_id (building_id), " +
-      "CONSTRAINT Zone_ibfk_1 FOREIGN KEY (building_id) REFERENCES Buildings (building_id) ON DELETE CASCADE ON UPDATE CASCADE)",
+      "CONSTRAINT `zoneFK1` FOREIGN KEY (`building_id`) REFERENCES `building` (`building_id`) ON DELETE CASCADE ON UPDATE CASCADE);",
   },
 ];
 
