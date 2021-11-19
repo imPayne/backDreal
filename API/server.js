253bc0ed9397e4a956a76264a0dabf78865eb77d
Mathieu Poilleux <mpoilleux@matferbourgeat.com>
2021-11-19 09:38:03 +0100

    refactored tableHandler.js

--- API/server.js
+++ API/server.js
@@ -1,148 +1,137 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 const csvFile = "./storages.csv";
-const mysql = require('mysql');
-const csv = require('csv-parser');
-const fs = require('fs');
+const mysql = require("mysql");
+const csv = require("csv-parser");
+const fs = require("fs");
+const init = require("./tableHandler");
+const { exit } = require("process");
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'MatferDreal61',
-  database: 'epytodo'
+init.createTable();
+
+/*const connection = mysql.createConnection({
+  host: "localhost",
+  user: "yornletard",
+  password: "8ebea8Lx1",
+  database: "dreal",
 });
 
 var zones = [];
 
-class Area{
+class Area {
   constructor(posX, posY, width, height) {
-      this._posX = posX;
-      this._posY = posY;
-      this._width = width;
-      this._height = height;
+    this._posX = posX;
+    this._posY = posY;
+    this._width = width;
+    this._height = height;
   }
   get posX() {
-      return this._posX;
+    return this._posX;
   }
   get posY() {
-      return this._posY;
+    return this._posY;
   }
   get width() {
-      return this._width;
+    return this._width;
   }
   get height() {
-      return this._height;
+    return this._height;
   }
 }
 
 class Zone extends Area {
   constructor(posX, posY, width, height, storages, alley, column) {
-      super(posX, posY, width, height);
-      this.storages = storages;
-      this.alley = alley;
-      this.column = column;
+    super(posX, posY, width, height);
+    this.storages = storages;
+    this.alley = alley;
+    this.column = column;
   }
   addStorage(storage) {
     var index = this.storages.findIndex(function (s) {
-      return(s.level === storage.level && s.storage === storage.storage);
+      return s.level === storage.level && s.storage === storage.storage;
     });
-    if (index == -1) 
-      this.storages.push(storage);
+    if (index == -1) this.storages.push(storage);
   }
 }
 
 class Storage {
   constructor(level, storage, posX, posY, masseBois, massePlastique, massePD) {
-      this.posX = posX;
-      this.posY = posY;
-      this.masseBois = masseBois;
-      this.massePlastique = massePlastique;
-      this.massePD = massePD;
-      this.level = level;
-      this.storage = storage;
+    this.posX = posX;
+    this.posY = posY;
+    this.masseBois = masseBois;
+    this.massePlastique = massePlastique;
+    this.massePD = massePD;
+    this.level = level;
+    this.storage = storage;
   }
 }
-
-function createTable() {
-
-  var sqlBuildings = 'CREATE TABLE IF NOT EXISTS Buildings (building_id int(11) NOT NULL, posX int(11) DEFAULT NULL, posY int(11) DEFAULT NULL, width int(11) DEFAULT NULL, height int(11) DEFAULT NULL, PRIMARY KEY (building_id))'; 
-  var sqlStorage = 'CREATE TABLE IF NOT EXISTS Storage (id int(11) DEFAULT NULL, zone_id int(11) DEFAULT NULL, level varchar(100) DEFAULT NULL, storage varchar(100) DEFAULT NULL, StorageMassBois int(11) DEFAULT NULL, StorageMassPlastique int(11) DEFAULT NULL, StorageMassPD int(11) DEFAULT NULL, UNIQUE KEY id (id), Key zone_id (zone_id), CONSTRAINT Storage_ibfk FOREIGN KEY (zone_id) REFERENCES Zone (id) ON DELETE CASCADE ON UPDATE CASCADE)';
-  var sqlZone = 'CREATE TABLE IF NOT EXISTS Zone (building_id int(11) NOT NULL, id int(11) NOT NULL AUTO_INCREMENT, zone_posX int(11) NOT NULL, zone_posY int(11) NOT NULL, zone_width int(11) DEFAULT NULL, zone_height int(11) DEFAULT NULL, alley varchar(45) DEFAULT NULL, `column` varchar(45) DEFAULT NULL, PRIMARY KEY (id), KEY building_id (building_id), CONSTRAINT Zone_ibfk_1 FOREIGN KEY (building_id) REFERENCES Buildings (building_id) ON DELETE CASCADE ON UPDATE CASCADE)';
-
-  connection.query(sqlBuildings);
-  connection.query(sqlZone);
-  connection.query(sqlStorage);
-  console.log("Table Buildings Zone and Storage successfully created");
-}
-createTable();
 
 function GetPreciseLocationY(row) {
-  var alleyNumber = +row.alley.match(/\d+/g);//.join('');
+  var alleyNumber = +row.alley.match(/\d+/g); //.join('');
   if (alleyNumber % 2 == 0) {
     var preciseY = parseFloat(row.Y, 10) + 20;
-  }
-  else {
+  } else {
     var preciseY = parseFloat(row.Y, 10) - 20;
   }
-  return(preciseY);
+  return preciseY;
 }
 
 function getBuildingId(row) {
   // building id return, il y a 4 buildings soit 4 id différent.
   var mainBuilding = 1;
   if (row.buildings == "FLO") {
-    return(mainBuilding);
+    return mainBuilding;
   }
 }
 
 //sendDataBuildings();
 
-
 function sendDataBuildings() {
   //permet d'initialiser la table Buildings dans la DB
   var query = "INSERT INTO Buildings SET ?";
   var array = [
-    {building_id: 1,posX: 90, posY: 50, width: 2315, height: 1040},
-    {building_id: 2,posX: 60, posY: 1150, width: 460, height: 530},
-    {building_id: 3,posX: 640, posY: 1150, width: 570, height: 590},
-    {building_id: 4,posX: 1335, posY: 1150, width: 405, height: 430},
-  ]
+    { building_id: 1, posX: 90, posY: 50, width: 2315, height: 1040 },
+    { building_id: 2, posX: 60, posY: 1150, width: 460, height: 530 },
+    { building_id: 3, posX: 640, posY: 1150, width: 570, height: 590 },
+    { building_id: 4, posX: 1335, posY: 1150, width: 405, height: 430 },
+  ];
 
   for (i in array) {
-    var params = {building_id: array[i].building_id, posX: array[i].posX, posY: array[i].posY, width: array[i].width, height: array[i].height};
+    var params = {
+      building_id: array[i].building_id,
+      posX: array[i].posX,
+      posY: array[i].posY,
+      width: array[i].width,
+      height: array[i].height,
+    };
     connection.query(query, params);
   }
   console.log("Insert in Buildings table success !");
 }
 
-function deleteOldData() {
-  var queryDeleteZone = "DELETE FROM `epytodo`.`Zone` WHERE (`id` >= '0');"
-  var queryDeleteStorage = "DELETE FROM `epytodo`.`Storage` WHERE (`id` >= '0');"
-  
-  connection.query(queryDeleteStorage);
-  connection.query(queryDeleteZone);
-  console.log("Delete Success !");
-}
-
-deleteOldData();
-
-
 fs.createReadStream(csvFile)
   .pipe(csv())
   .on("data", (row) => {
     //create buildings in Buildings table
-    var preciseY =  GetPreciseLocationY(row);
+    var preciseY = GetPreciseLocationY(row);
     var rowX = parseInt(row.X, 10);
-    row.column = '' ? 'none' : row.column;
+    row.column = "" ? "none" : row.column;
     var zone = new Zone(rowX, preciseY, 20, 20, [], row.alley, row.column);
     var storage = new Storage(row.level, row.storage, 0, 0, 0, 0, 0);
 
     var buildingNumberId = getBuildingId(row);
-    
 
     //set prerequis query
-    var paramsZone = {building_id: buildingNumberId,zone_posX: rowX, zone_posY: preciseY, zone_width: 20, zone_height: 20, alley: row.alley, column: row.column};
+    var paramsZone = {
+      building_id: buildingNumberId,
+      zone_posX: rowX,
+      zone_posY: preciseY,
+      zone_width: 20,
+      zone_height: 20,
+      alley: row.alley,
+      column: row.column,
+    };
     var queryZone = "INSERT INTO Zone SET ?";
     var queryStorage = "INSERT INTO Storage SET ?";
     var storageZoneId;
@@ -155,28 +144,37 @@ fs.createReadStream(csvFile)
       zone.addStorage(storage);
       zones.push(zone);
       connection.query(queryZone, paramsZone);
-    }
-    else {
+    } else {
       zones[index].addStorage(storage);
     }
-    connection.query("SELECT `id` FROM Zone WHERE ?", [{alley: row.alley}, {column: row.column}], function(err, result, fields) {
-
-      if (err) throw err;
+    connection.query(
+      "SELECT `id` FROM Zone WHERE ?",
+      [{ alley: row.alley }, { column: row.column }],
+      function (err, result, fields) {
+        if (err) throw err;
         storageZoneId = result[0].id;
         console.log(storageZoneId);
-    }); //récupère l'id Zone grâce à l'alley et le current alley 
-    var paramsStorage = {zone_id: storageZoneId,level: row.level, storage: row.storage};
+      }
+    ); //récupère l'id Zone grâce à l'alley et le current alley
+    var paramsStorage = {
+      zone_id: storageZoneId,
+      level: row.level,
+      storage: row.storage,
+    };
     connection.query(queryStorage, paramsStorage);
   })
-  
+
   .on("end", () => {
-    console.log("CSV file successfully processed and Data inserted in Zone and Storage");
+    console.log(
+      "CSV file successfully processed and Data inserted in Zone and Storage"
+    );
   });
 
 app.get("/", (req, res) => {
-  res.json({message: 'Root page ready'})
-})
+  res.json({ message: "Root page ready" });
+});
 
 app.listen(port, () => {
   console.log(`Running at port ${port}`);
-})
+});
+*/
--- API/tableHandler.js
+++ API/tableHandler.js
@@ -1,27 +1,67 @@
-const express = require('express');
-const mysql = require('mysql');
-const fs = require('fs');
-const port = 8000;
-const app = express();
-
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'VOTRE MDP',
-    database: 'NOM DE BASE'
-  });
+  host: "localhost",
+  user: "yornletard",
+  password: "8ebea8Lx1",
+  database: "dreal",
+});
+
+const queries = [
+  {
+    name: "building",
+    sql:
+      "CREATE TABLE IF NOT EXISTS Buildings (" +
+      "building_id int(11) NOT NULL, " +
+      "posX int(11) DEFAULT NULL, " +
+      "posY int(11) DEFAULT NULL, " +
+      "width int(11) DEFAULT NULL, " +
+      "height int(11) DEFAULT NULL, " +
+      "PRIMARY KEY (building_id))",
+  },
+  {
+    name: "storage",
+    sql:
+      "CREATE TABLE IF NOT EXISTS Storage ( " +
+      "id int(11) DEFAULT NULL, " +
+      "zone_id int(11) DEFAULT NULL, " +
+      "level varchar(100) DEFAULT NULL, " +
+      "storage varchar(100) DEFAULT NULL, " +
+      "StorageMassBois int(11) DEFAULT NULL, " +
+      "StorageMassPlastique int(11) DEFAULT NULL, " +
+      "StorageMassPD int(11) DEFAULT NULL, " +
+      "UNIQUE KEY id (id), Key zone_id (zone_id), " +
+      "CONSTRAINT Storage_ibfk FOREIGN KEY (zone_id) REFERENCES Zone (id) ON DELETE CASCADE ON UPDATE CASCADE)",
+  },
+  {
+    name: "zone",
+    sql:
+      "CREATE TABLE IF NOT EXISTS Zone (" +
+      "building_id int(11) NOT NULL, " +
+      "id int(11) NOT NULL AUTO_INCREMENT, " +
+      "zone_posX int(11) NOT NULL, " +
+      "zone_posY int(11) NOT NULL, " +
+      "zone_width int(11) DEFAULT NULL, " +
+      "zone_height int(11) DEFAULT NULL, " +
+      "alley varchar(45) DEFAULT NULL, " +
+      "`column` varchar(45) DEFAULT NULL, " +
+      "PRIMARY KEY (id), " +
+      "KEY building_id (building_id), " +
+      "CONSTRAINT Zone_ibfk_1 FOREIGN KEY (building_id) REFERENCES Buildings (building_id) ON DELETE CASCADE ON UPDATE CASCADE)",
+  },
+];
 
 function createTable() {
-
-    var sqlBuildings = 'CREATE TABLE IF NOT EXISTS Buildings (building_id int(11) NOT NULL, posX int(11) DEFAULT NULL, posY int(11) DEFAULT NULL, width int(11) DEFAULT NULL, height int(11) DEFAULT NULL, PRIMARY KEY (building_id))'; 
-    var sqlStorage = 'CREATE TABLE IF NOT EXISTS Storage (id int(11) DEFAULT NULL, zone_id int(11) DEFAULT NULL, level varchar(100) DEFAULT NULL, storage varchar(100) DEFAULT NULL, StorageMassBois int(11) DEFAULT NULL, StorageMassPlastique int(11) DEFAULT NULL, StorageMassPD int(11) DEFAULT NULL, UNIQUE KEY id (id), Key zone_id (zone_id), CONSTRAINT Storage_ibfk FOREIGN KEY (zone_id) REFERENCES Zone (id) ON DELETE CASCADE ON UPDATE CASCADE)';
-    var sqlZone = 'CREATE TABLE IF NOT EXISTS Zone (building_id int(11) NOT NULL, id int(11) NOT NULL AUTO_INCREMENT, zone_posX int(11) NOT NULL, zone_posY int(11) NOT NULL, zone_width int(11) DEFAULT NULL, zone_height int(11) DEFAULT NULL, alley varchar(45) DEFAULT NULL, `column` varchar(45) DEFAULT NULL, PRIMARY KEY (id), KEY building_id (building_id), CONSTRAINT Zone_ibfk_1 FOREIGN KEY (building_id) REFERENCES Buildings (building_id) ON DELETE CASCADE ON UPDATE CASCADE)';
-  
-    connection.query(sqlBuildings);
-    connection.query(sqlZone);
-    connection.query(sqlStorage);
-    console.log("Table Buildings Zone and Storage successfully created");
-  }
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    queries.forEach((query) => {
+      connection.query(query.sql, function (err, result) {
+        if (err) throw err;
+        console.log(`Table ${query.name} created`);
+      });
+    });
+  });
+}
 
 module.exports = { createTable };
