168ce920a30c56e014648eb0a59915a76b8e1044
monchablon hugo <mhugo@localhost.localdomain>
2021-11-19 08:27:05 +0100

    push tableHandler.js check if db table exist if not the function create it

--- API/server.js
+++ API/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const csvFile = "../csv/storages.csv";
+const csvFile = "./storages.csv";
 const mysql = require('mysql');
 const csv = require('csv-parser');
 const fs = require('fs');
@@ -63,6 +63,19 @@ class Storage {
       this.storage = storage;
   }
 }
+
+function createTable() {
+
+  var sqlBuildings = 'CREATE TABLE IF NOT EXISTS Buildings (building_id int(11) NOT NULL, posX int(11) DEFAULT NULL, posY int(11) DEFAULT NULL, width int(11) DEFAULT NULL, height int(11) DEFAULT NULL, PRIMARY KEY (building_id))'; 
+  var sqlStorage = 'CREATE TABLE IF NOT EXISTS Storage (id int(11) DEFAULT NULL, zone_id int(11) DEFAULT NULL, level varchar(100) DEFAULT NULL, storage varchar(100) DEFAULT NULL, StorageMassBois int(11) DEFAULT NULL, StorageMassPlastique int(11) DEFAULT NULL, StorageMassPD int(11) DEFAULT NULL, UNIQUE KEY id (id), Key zone_id (zone_id), CONSTRAINT Storage_ibfk FOREIGN KEY (zone_id) REFERENCES Zone (id) ON DELETE CASCADE ON UPDATE CASCADE)';
+  var sqlZone = 'CREATE TABLE IF NOT EXISTS Zone (building_id int(11) NOT NULL, id int(11) NOT NULL AUTO_INCREMENT, zone_posX int(11) NOT NULL, zone_posY int(11) NOT NULL, zone_width int(11) DEFAULT NULL, zone_height int(11) DEFAULT NULL, alley varchar(45) DEFAULT NULL, `column` varchar(45) DEFAULT NULL, PRIMARY KEY (id), KEY building_id (building_id), CONSTRAINT Zone_ibfk_1 FOREIGN KEY (building_id) REFERENCES Buildings (building_id) ON DELETE CASCADE ON UPDATE CASCADE)';
+
+  connection.query(sqlBuildings);
+  connection.query(sqlZone);
+  connection.query(sqlStorage);
+  console.log("Table Buildings Zone and Storage successfully created");
+}
+createTable();
 
 function GetPreciseLocationY(row) {
   var alleyNumber = +row.alley.match(/\d+/g);//.join('');
@@ -152,7 +165,6 @@ fs.createReadStream(csvFile)
         storageZoneId = result[0].id;
         console.log(storageZoneId);
     }); //récupère l'id Zone grâce à l'alley et le current alley 
-    console.log(storageZoneId);
     var paramsStorage = {zone_id: storageZoneId,level: row.level, storage: row.storage};
     connection.query(queryStorage, paramsStorage);
   })
